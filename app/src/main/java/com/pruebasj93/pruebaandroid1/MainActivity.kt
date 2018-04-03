5857baead5256e0df2bf626b776d8a76cf7471ce
Jeovani Martínez <jjmartinez@storecheck.com>
2018-04-03 11:22:08 -0500

    Add message

--- app/src/main/java/com/pruebasj93/pruebaandroid1/MainActivity.kt
+++ app/src/main/java/com/pruebasj93/pruebaandroid1/MainActivity.kt
@@ -2,11 +2,17 @@ package com.pruebasj93.pruebaandroid1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.alert
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        alert("Hola")
+        {
+            positiveButton("Aceptar"){}
+        }.show()
     }
 }
